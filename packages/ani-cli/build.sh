669d0588f322ab6224b9e8df7b3c326dd80be6d7
djunekz <gab288.gab288@passinbox.com>
2026-04-30 10:26:52 +0700

    Pkg: update ani-cli 4.11 to 4.13

--- packages/ani-cli/build.sh
+++ packages/ani-cli/build.sh
@@ -2,10 +2,10 @@ TERMUX_PKG_HOMEPAGE=https://github.com/pystardust/ani-cli
 TERMUX_PKG_DESCRIPTION="A cli tool to browse and play anime"
 TERMUX_PKG_LICENSE="GPL-3.0"
 TERMUX_PKG_MAINTAINER="@termux-app-store"
-TERMUX_PKG_VERSION=4.11
+TERMUX_PKG_VERSION=4.13
 TERMUX_PKG_SRCURL=https://github.com/pystardust/ani-cli/releases/download/v${TERMUX_PKG_VERSION}/ani-cli
-TERMUX_PKG_SHA256=1f35c47b0c6d924261d096663d16b03d489f488fcc38c74e4fe87b45a330b7cf
-TERMUX_PKG_DEPENDS="nodejs, fzf"
+TERMUX_PKG_SHA256=17c8a97b034d3e48ba4663d538a5705adfbbffb951fa67650a57f7e4414959e2
+TERMUX_PKG_DEPENDS="nodejs, aria2, ffmpeg, fzf, grep, sed, wget"
 
 termux_step_make_install() {
     npm install -g --prefix "$TERMUX_PREFIX" "$TERMUX_PKG_SRCDIR"
